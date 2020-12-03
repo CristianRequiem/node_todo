95b88c508a175dbf30a93d0542b7a29a88045664
Cristian <cristianrequiem@yahoo.es>
2020-12-02 18:59:40 -0600

    Primer commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Aplicación de comandos de Node JS para TODOs
+
+Aplicación para creación de TODOs con el uso de comandos con yargs
+
+Instalar paquetes de Node
+```
+npm install
+```
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+const { parse } = require('querystring');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+    let data = JSON.stringify(listadoPorHacer);
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) return new Error('No se pudo guardar la tarea', err);
+    });
+}
+
+const cargarDB = () => {
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+}
+
+const crear = (descripcion) => {
+    cargarDB();
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+
+    guardarDB();
+
+    return porHacer;
+}
+
+const getListado = (filtrar) => {
+    cargarDB();
+    if (!filtrar) {
+        return listadoPorHacer;
+    } else {
+        let listadoConFiltro = listadoPorHacer.filter(tarea => tarea.completado.toString() == filtrar);
+        return listadoConFiltro;
+    }
+}
+
+const actualizar = (descripcion, completado = true) => {
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const borrar = (descripcion) => {
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+    console.log(index);
+    if (index >= 0) {
+        listadoPorHacer.splice(index, 1);
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
